e6d4d533a06d7330cedc87ad20961d1d0e54db70
Diogo Melo <demelodiogo@hotmail.com>
2025-04-14 18:10:47 +0100

    Criação das rotas novas para o portfólio e para o currículo

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/curriculo")
+def curriculo():
+    return render_template("curriculo.html")
+
+@main.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
